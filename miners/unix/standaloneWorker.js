d773d2d73cef6ab041b2f87efb079cf659b54d78
Wes Garland <wgarland@sparc.network>
2018-06-15 10:42:59 -0400

    unix miner - js code edited for style

--- miners/unix/standaloneWorker.js
+++ miners/unix/standaloneWorker.js
@@ -7,36 +7,36 @@
  *  @date       March 2018
  */
 
-/** Module configuration parameters. May be altered at runtime. Should be altered 
+/** Module configuration parameters. May be altered at runtime. Should be altered
  *  before first Worker is instanciated.
  */
-exports.config =
-{
-  debug:                true,                   /**< When false, console.debug is NOP */
-  debugLevel:           3,                      /**< Bigger = more verbose */
-  defaultHostname:      "127.0.0.1",
-  defaultService:       "9000",
-  docRoot:              "/var/dcp/www/docs"
-};
+exports.config = {
+  debug: false, /* When false, console.debug is NOP */
+  debugLevel: 3, /* Bigger = more verbose */
+  defaultHostname: '127.0.0.1',
+  defaultService: '9000',
+  docRoot: '/var/dcp/www/docs'
+}
 
-console.debug = function()
-{
-  if (!exports.config.debug)
-    return;
+console.debug = function () {
+  if (!exports.config.debug) {
+    return
+  }
 
-  console.log.apply(null, arguments);
-};
+  console.log.apply(null, arguments)
+}
 
-if (!exports.config.debug)
-  console.debug = function(){};
+if (!exports.config.debug) {
+  console.debug = function () {}
+}
 
 /** Worker constructor
  *  @param      filename        The filename of the code to run in the worker, relative to exports.config.docRoot.
- *  @param      hostname        The hostname (or IP number) of the standalone miner process. 
+ *  @param      hostname        The hostname (or IP number) of the standalone miner process.
  *  @param      service         The service (or port number) of the standalone miner process.
  *
  *  @returns an object with the following
- *  - methods: 
+ *  - methods:
  *    . addEventListener
  *    . removeEventListener
  *    . postMessage
@@ -44,180 +44,151 @@ if (!exports.config.debug)
  *    . create
  *  - properties:
  *    . onmessage
- *    . onerror 
+ *    . onerror
  *  - events:
  *    . error
  *    . messeage
  */
-exports.Worker = function standaloneWorker$$Worker(filename, hostname, service)
-{
-  var   socket = new (require("net")).Socket();
-  var   ee = new (require('events').EventEmitter)();
-  var   pendingWrites = [];
-  var   readBuf = "";
-  var   connected = false;
-  var   dieTimer;
-  var   code;
+exports.Worker = function standaloneWorker$$Worker (filename, hostname, service) {
+  var socket = new (require('net')).Socket()
+  var ee = new (require('events').EventEmitter)()
+  var pendingWrites = []
+  var readBuf = ''
+  var connected = false
+  var dieTimer
+  var code
 
-  if (typeof filename != "string")
-    throw new TypeError("filename must be a string!");
-  if (filename[0] === ".")
-    throw new Error("relative paths not allowed (security)");
-  code = require("fs").readFileSync(exports.config.docRoot + "/" + (filename.replace(/\?.*$/,"")), "utf-8");
+  if (typeof filename !== 'string') { throw new TypeError('filename must be a string!') }
+  if (filename[0] === '.') { throw new Error('relative paths not allowed (security)') }
+  code = require('fs').readFileSync(exports.config.docRoot + '/' + (filename.replace(/\?.*$/, '')), 'utf-8')
 
-  this.addEventListener    = ee.addListener.bind(ee);
-  this.removeEventListener = ee.removeListener.bind(ee);
-  
-  function finishConnect()
-  {
-    var  wrappedMessage = JSON.stringify({ type: "initWorker", payload: code }) + "\n";
+  this.addEventListener = ee.addListener.bind(ee)
+  this.removeEventListener = ee.removeListener.bind(ee)
 
-    connected = true;
-    socket.setEncoding("ascii");
-    console.debug("Connected; " + pendingWrites.length + " pending messages.");
-    
-    /* We buffer writes in pendingWrites between the call to connect() and 
+  function finishConnect () {
+    var wrappedMessage = JSON.stringify({ type: 'initWorker', payload: code }) + '\n'
+
+    connected = true
+    socket.setEncoding('ascii')
+    console.debug('Connected ' + pendingWrites.length + ' pending messages.')
+
+    /* We buffer writes in pendingWrites between the call to connect() and
      * the actual establishment of the TCP socket. Once connected, we drain that
      * buffer into the write buffer in Node's net module.
      */
-    if (connected)
-      socket.write(wrappedMessage);
-    else
-      pendingWrites.push(wrappedMessage);
+    if (connected) {
+      socket.write(wrappedMessage)
+    } else {
+      pendingWrites.push(wrappedMessage)
+    }
 
-    while (pendingWrites.length && !socket.destroyed)
-      socket.write(pendingWrites.shift());
+    while (pendingWrites.length && !socket.destroyed) {
+      socket.write(pendingWrites.shift())
+    }
   }
-  
+
   /** Receive data from the network, turning them into debug output,
    *  remote exceptions, worker messages, etc.
    */
-  socket.on("data", function standaloneWorker$$Worker$recvData(data)
-  {
-    var line, lineObj;  /* line of data coming over the network */
-    var nl;
+  socket.on('data', function standaloneWorker$$Worker$recvData (data) {
+    var line, lineObj /* line of data coming over the network */
+    var nl
 
-    readBuf += data;
-    while ((nl = readBuf.indexOf("\n")) != -1)
-    {
-      try
-      {
-        line = readBuf.slice(0,nl);
-        readBuf = readBuf.slice(nl+1);
-  
-        if (!line.length)
-          continue;
+    readBuf += data
+    while ((nl = readBuf.indexOf('\n')) !== -1) {
+      try {
+        line = readBuf.slice(0, nl)
+        readBuf = readBuf.slice(nl + 1)
 
-        if (line.match(/^DIE: */))
-        {
+        if (!line.length) { continue }
+
+        if (line.match(/^DIE: */)) {
           /* Remote telling us they are dying */
-          if (exports.config.debugLevel > 2)
-            console.debug("Remote is dying (", line + ")");
-          socket.destroy();
-          clearTimeout(dieTimer);
-          break;
+          if (exports.config.debugLevel > 2) { console.debug('Remote is dying (', line + ')') }
+          socket.destroy()
+          clearTimeout(dieTimer)
+          break
         }
 
-        if (!line.match(/^MSG: */))
-        {
-          if (exports.config.debugLevel > 2)
-            console.debug("remote:", line);
-          continue;
+        if (!line.match(/^MSG: */)) {
+          if (exports.config.debugLevel > 2) { console.debug('remote:', line) }
+          continue
         }
 
-        lineObj = JSON.parse(line.slice(4));
-        if (lineObj.hasOwnProperty("message"))
-        {
+        lineObj = JSON.parse(line.slice(4))
+        if (lineObj.hasOwnProperty('message')) {
           /* Remote posted message */
-          ee.emit("message", {data: lineObj.message});
-        }
-        else
-        {
-          if (lineObj.hasOwnProperty("exception"))
-          {
+          ee.emit('message', {data: lineObj.message})
+        } else {
+          if (lineObj.hasOwnProperty('exception')) {
             /* Remote threw exception */
-            lineObj.exception.type = "Remote" + lineObj.type;
-	    console.log("Remote exception: ", lineObj.exception.stack);
-            ee.emit("error", lineObj.exception);
-            continue;
+            lineObj.exception.type = 'Remote' + lineObj.type
+            console.log('Remote exception: ', lineObj.exception.stack)
+            ee.emit('error', lineObj.exception)
+            continue
           }
-
-          ee.emit("error", new Error("Unrecognized command from remote, '" + line + "'"));
+          ee.emit('error', new Error('Unrecognized command from remote, \'' + line + '\''))
         }
-      }
-      catch(e)
-      {
-        console.error("Error processing remote response: '" + line + "' (" + e.name + ": " + e.message + e.stack.split("\n")[1].replace(/^  */, " ") + ")");
-        throw e;
+      } catch (e) {
+        console.error('Error processing remote response: \'' + line + '\' (' + e.name + ': ' + e.message + e.stack.split('\n')[1].replace(/^  */, ' ') + ')')
+        throw e
       }
     }
-  });
+  })
 
-  socket.on("error", function worker$$Error(e)
-  {
-    console.error("Error communicating with worker: ", e);
-    socket.destroy();
-    throw e;
-  });
+  socket.on('error', function worker$$Error (e) {
+    console.error('Error communicating with worker: ', e)
+    socket.destroy()
+    throw e
+  })
 
-  socket.on("close", function worker$$Close()
-  {
-    console.debug("Closed socket");
-  });
+  socket.on('close', function worker$$Close () {
+    console.debug('Closed socket')
+  })
 
-  console.debug("Connecting to", (hostname || exports.config.defaultHostname) + ":" + (service || exports.config.defaultService));
-  socket.connect(service || exports.config.defaultService, hostname || exports.config.defaultHostname, finishConnect);
-  
+  console.debug('Connecting to', (hostname || exports.config.defaultHostname) + ':' + (service || exports.config.defaultService))
+  socket.connect(service || exports.config.defaultService, hostname || exports.config.defaultHostname, finishConnect)
+
   /** Send a message over the network to a standalone worker */
-  this.postMessage = function standaloneWorker$$Worker$postMessage(message)
-  {
-    var wrappedMessage = JSON.stringify({ type: "workerMessage", message: message }) + "\n";
-    if (connected)
-      socket.write(wrappedMessage);
-    else
-      pendingWrites.push(wrappedMessage);
+  this.postMessage = function standaloneWorker$$Worker$postMessage (message) {
+    var wrappedMessage = JSON.stringify({ type: 'workerMessage', message: message }) + '\n'
+    if (connected) { socket.write(wrappedMessage) } else { pendingWrites.push(wrappedMessage) }
   }
 
-  /** The the worker to die.  The worker should respond with a message back of 
+  /** The the worker to die.  The worker should respond with a message back of
    *  type DIE:, which in turn eventuallys triggers socket.close() and .destroy()
    */
-  this.terminate = function Worker$$terminate()
-  {
-    var wrappedMessage = JSON.stringify({ type: "die" }) + "\n";
-    if (connected)
-      socket.write(wrappedMessage);
-    else
-      pendingWrites.push(wrappedMessage);
+  this.terminate = function Worker$$terminate () {
+    var wrappedMessage = JSON.stringify({ type: 'die' }) + '\n'
+    if (connected) { socket.write(wrappedMessage) } else { pendingWrites.push(wrappedMessage) }
 
     /* If DIE: response doesn't arrive in a reasonable time -- clean up */
-    function cleanup()
-    {
-      socket.destroy();
+    function cleanup () {
+      socket.destroy()
     }
-    dieTimer = setTimeout(cleanup, 7000);
+    dieTimer = setTimeout(cleanup, 7000)
   }
 }
 
 /* Attach setters for onmessage, onerror, etc on the Worker.prototype
  * which are implemented with this.addEventListener.
- */    
-const onHandlerTypes = ["message", "error" ];
-exports.Worker.prototype.onHandlers={};
+ */
+const onHandlerTypes = ['message', 'error']
+exports.Worker.prototype.onHandlers = {}
 
-for (let i=0; i < onHandlerTypes.length; i++)
-{
-  let onHandlerType = onHandlerTypes[i];
-  Object.defineProperty(exports.Worker.prototype, "on" + onHandlerType,
-  {
-    enumerable:   true,
+for (let i = 0; i < onHandlerTypes.length; i++) {
+  let onHandlerType = onHandlerTypes[i]
+  Object.defineProperty(exports.Worker.prototype, 'on' + onHandlerType, {
+    enumerable: true,
     configurable: false,
-    set: function(cb)
-    {
+    set: function (cb) {
       /* maintain on{eventName} singleton pattern */
-      if (this.onHandlers.hasOwnProperty(onHandlerType))
-	this.removeEventListener(onHandlerType, this.onHandlers[onHandlerType]);
-      this.addEventListener(onHandlerType, cb);
-      this.onHandlers[onHandlerType] = cb;
+      if (this.onHandlers.hasOwnProperty(onHandlerType)) { this.removeEventListener(onHandlerType, this.onHandlers[onHandlerType]) }
+      this.addEventListener(onHandlerType, cb)
+      this.onHandlers[onHandlerType] = cb
     },
-  });
+    get: function () {
+      return this.onHandlers[onHandlerType]
+    }
+  })
 }
