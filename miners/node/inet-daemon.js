7900232c1ffd6e0ade13daaad4ea98da9a704a9b
Wes Garland <wgarland@sparc.network>
2018-06-27 10:09:31 -0400

    standalone node miner - misc networking, global symbol bug fixes, now works with worker repl. Improved debug output and especially stack dumps in miner control

--- miners/node/inet-daemon.js
+++ miners/node/inet-daemon.js
@@ -2,7 +2,7 @@
 
 /** Simple daemon for node that works like xinetd/inetd from the worker's POV */
 
-var debug = false
+var debug = process.env.NODE_MINER_DEBUG || ""
 var config = {
   listen_port: process.env.NODE_MINER_LISTEN_PORT || '9000',
   listen_host: process.env.NODE_MINER_LISTEN_HOST || '127.0.0.1',
@@ -18,36 +18,61 @@ function handleConnection(socket) {
   if (debug)
     console.log("Handling new connection")
   child = require("child_process").spawn(config.node_path, [require.resolve("./dcp-miner-node.js")])
-  child.stdin.setEncoding("ascii");
-  child.stderr.setEncoding("ascii");
-  socket.setEncoding("ascii");
+  child.stdin.setEncoding("ascii")
+  child.stdout.setEncoding("ascii")
+  child.stderr.setEncoding("ascii")
+  socket.setEncoding("ascii")
 
   socket.on("end", function() {
     if (debug)
-      console.log("Killing miner")
+      console.log("Killing worker")
     child.kill()
   })
 
   socket.on("error", function(e) {
+    console.log("Error from supervisor:", e)
+    socket.destroy()
+    child.end()
+  })
+
+  child.on("error", function(e) {
+    console.log("Error from worker:", e)
     socket.destroy()
     child.end()
   })
 
   child.on("exit", function(code) {
     if (debug)
-      console.log("Miner exited")
-    socket.destroy();
+      console.log("worker exited; closing socket", code ? code : "")
+    socket.end()
+    socket.destroy()
   })
 
   child.stdout.on("data", function(data) {
+    if (debug.indexOf("network") !== -1) {
+      console.log("> ", data.replace(/\n/, "\u2424"))
+      if (data.length > 100)
+	console.log('\n')
+    }
     socket.write(data)
   })
 
   child.stderr.on("data", function(data) {
-    console.log("stderr: ", data)
+    console.log("worker stderr: ", data)
   })
 
   socket.on("data", function(data) {
+    if (debug.indexOf("network") !== -1) {
+      console.log("< ", data.replace(/\n/, "\u2424"))
+      if (data.length > 100)
+	console.log('\n')
+    }
     child.stdin.write(data)
   })
 }
+
+process.on('uncaughtException', function (e) {
+  console.log('\n---',(new Date()).toLocaleString(),'-------------------------------------------------')
+  console.log('uncaught exception:', e.stack)
+  console.log('\n')
+})
