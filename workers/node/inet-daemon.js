a941b2c74a916b9607a5f274f8725ebe9b5da463
Wes Garland <wgarland@sparc.network>
2018-07-26 15:36:37 -0400

    Merge branch 'DCP-71-add-configuration-file-for-dcp-applications' of gitlab.com:Distributed-Compute-Protocol/dcp-testing into DCP-71-add-configuration-file-for-dcp-applications

--- workers/node/inet-daemon.js
+++ workers/node/inet-daemon.js
@@ -1,12 +1,12 @@
 #! /usr/bin/env node
-/** 
- *  @file       inet-daemon.js          Simple daemon for node that works like xinetd/inetd 
+/**
+ *  @file       inet-daemon.js          Simple daemon for node that works like xinetd/inetd
  *
  *                                      Configuration is read from the dcp-config.js file
  *                                      and its descendendents. Services are configured thus:
  *
  *                                       inetDaemon: {
- *                                         label: { 
+ *                                         label: {
  *                                           net:       {
  *                                             port: port,
  *                                             listenHost: optional,
@@ -23,99 +23,86 @@
 /* global dcpConfig */
 const net = require('net')
 var debug = process.env.DEBUG || ''
-
+// console.log(process.env)
 require('dcp-rtlink/rtLink').link(module.paths)
 require('config').load()
 
-if (!process.env.FORKED) {
-  start()
-} else {
-  process.on('message', message => {
-    if (message.type === 'config') {
-      dcpConfig = message.config
-      start()
+Object.entries(dcpConfig.inetDaemon).forEach(function (param) {
+  var [name, config] = param
+  var server = net.createServer(handleConnection)
+
+  if (debug.indexOf('verbose') !== -1) {
+    console.log('Listening for ' + name + ' connections on ' + (config.net.listenHost || 'inaddr_any') + ':' + config.net.port)
+  }
+  server.listen({port: config.net.port, host: config.net.listenHost}, () => {
+    // To let tests know we've actually started
+    if (process.env.FORKED) {
+      process.send({
+        request: 'Server Started',
+        config
+      })
     }
   })
-}
 
-function start () {
-  Object.entries(dcpConfig.inetDaemon).forEach(function (param) {
-    var [name, config] = param
-    var server = net.createServer(handleConnection)
-    
-    if (debug.indexOf('verbose') !== -1) {
-      console.log('Listening for ' + name + ' connections on ' + (config.net.listenHost || 'inaddr_any') + ':' + config.net.port)
-    }
-    server.listen({port: config.net.port, host: config.net.listenHost}, () => {
-      // To let tests know we've actually started
-      if (process.env.FORKED) {
-        process.send({
-  	request: 'Server Started',
-  	config
-        })
+  function handleConnection (socket) {
+    if (debug.indexOf('verbose') !== -1) { console.log('New connection; spawning ', config.process, config.arguments) }
+    var child = require('child_process').spawn(config.process, config.arguments || [])
+    if (debug) { console.log('Spawned a new worker process, PID:', child.pid) }
+
+    child.stderr.setEncoding('ascii')
+
+    socket.on('end', function () {
+      if (debug) { console.log('Killing worker') }
+      child.kill()
+    })
+
+    socket.on('error', function (e) {
+      console.log('Error from supervisor:', e)
+      socket.destroy()
+      child.kill()
+    })
+
+    child.on('error', function (e) {
+      console.log('Error from worker:', e)
+      socket.destroy()
+      child.kill()
+    })
+
+    child.on('exit', function (code) {
+      if (debug) { console.log('worker exited; closing socket', code || '') }
+      socket.end()
+      socket.destroy()
+    })
+
+    child.stdout.on('data', function (data) {
+      if (debug.indexOf('network') !== -1) {
+        console.log('<W ', bufToDisplayStr(data))
+        if (data.length > 100) {
+          console.log('\n')
+        }
+      }
+      socket.write(data)
+    })
+
+    child.stderr.on('data', function (data) {
+      console.log('worker stderr: ', data)
+    })
+
+    socket.on('data', function (data) {
+      if (debug.indexOf('network') !== -1) {
+        console.log('W> ', bufToDisplayStr(data).slice(0, 64), '...')
+      }
+      try {
+        child.stdin.write(data)
+      } catch (e) {
+        console.log('could not write to worker process (', child.pid, ') stdin')
+        throw e
       }
     })
 
-    function handleConnection (socket) {
-      if (debug.indexOf('verbose') !== -1) { console.log("New connection; spawning ", config.process, config.arguments) }
-      var child = require('child_process').spawn(config.process, config.arguments || [])
-      if (debug) { console.log('Spawned a new worker process, PID:', child.pid) }
-
-      child.stderr.setEncoding('ascii')
-
-      socket.on('end', function () {
-        if (debug) { console.log('Killing worker') }
-        child.kill()
-      })
-
-      socket.on('error', function (e) {
-        console.log('Error from supervisor:', e)
-        socket.destroy()
-        child.kill()
-      })
-
-      child.on('error', function (e) {
-        console.log('Error from worker:', e)
-        socket.destroy()
-        child.kill()
-      })
-
-      child.on('exit', function (code) {
-        if (debug) { console.log('worker exited; closing socket', code || '') }
-        socket.end()
-        socket.destroy()
-      })
-
-      child.stdout.on('data', function (data) {
-        if (debug.indexOf('network') !== -1) {
-  	console.log('<W ', bufToDisplayStr(data))
-  	if (data.length > 100) {
-            console.log('\n')
-  	}
-        }
-        socket.write(data)
-      })
-
-      child.stderr.on('data', function (data) {
-        console.log('worker stderr: ', data)
-      })
-
-      socket.on('data', function (data) {
-        if (debug.indexOf('network') !== -1) {
-  	console.log('W> ', bufToDisplayStr(data).slice(0, 64), '...')
-        }
-        try {
-  	child.stdin.write(data)
-        } catch (e) {
-  	console.log('could not write to worker process (', child.pid, ') stdin')
-  	throw e
-        }
-      })
-
-      if (debug) { console.log('Handling new connection') }
-    }
-  })
-}
+    if (debug) { console.log('Handling new connection') }
+  }
+})
 
 function bufToDisplayStr (buf) {
   return Buffer.from(buf.toString('utf-8').replace(/\n/, '\u2424')).toString('utf-8')
