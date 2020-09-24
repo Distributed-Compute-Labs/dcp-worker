bc637f98e7187d5c0ce505a77582ddb1950675b7
Levi Stringer <levi@kingsds.network>
2020-09-24 15:51:07 -0400

    Updated naming conventions

---
+++ evaluator-setup-files.js
@@ -0,0 +1,35 @@
+exports.node = [
+    "dcp-worker/libexec/evaluator/sa-ww-simulation.js",
+    "dcp-worker/libexec/evaluator/script-load-wrapper.js",
+    "dcp-worker/libexec/evaluator/primitive-timers.js",
+    "dcp-worker/libexec/evaluator/event-loop-virtualization.js",
+    "dcp-worker/libexec/evaluator/access-lists.js",
+    "dcp-worker/libexec/evaluator/bravojs-init.js",
+    "bravojs/bravo.js",
+    "dcp-worker/libexec/evaluator/bravojs-env.js",
+    "dcp-worker/libexec/evaluator/calculate-capabilities.js",
+    "dcp-worker/libexec/evaluator/bootstrap.js"
+  ];
+exports.browser = [
+    "dcp-worker/libexec/evaluator/script-load-wrapper.js",
+    "dcp-worker/libexec/evaluator/primitive-timers.js",
+    "dcp-worker/libexec/evaluator/event-loop-virtualization.js",
+    "dcp-worker/libexec/evaluator/access-lists.js",
+    "dcp-worker/libexec/evaluator/bravojs-init.js",
+    "bravojs/bravo.js",
+    "dcp-worker/libexec/evaluator/bravojs-env.js",
+    "dcp-worker/libexec/evaluator/calculate-capabilities.js",
+    "dcp-worker/libexec/evaluator/bootstrap.js"
+];
+
+exports.native = [
+    "dcp-worker/libexec/evaluator/sa-ww-simulation",
+    "dcp-worker/libexec/evaluator/script-load-wrapper",
+    "dcp-worker/libexec/evaluator/event-loop-virtualization",
+    "dcp-worker/libexec/evaluator/access-lists",
+    "dcp-worker/libexec/evaluator/bravojs-init",
+    "bravojs/bravo",
+    "dcp-worker/libexec/evaluator/bravojs-env",
+    "dcp-worker/libexec/evaluator/calculate-capabilities",
+    "dcp-worker/libexec/evaluator/bootstrap"
+];
