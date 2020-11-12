836eadc0a9279709030467ffd9fc657e516859e3
SamCantor <samcantor@kingsds.network>
2020-11-12 14:19:50 -0500

    Recovered workerv4 patch manually

---
+++ evaluator-setup-files.js
@@ -0,0 +1,35 @@
+exports.node = [
+  "sa-ww-simulation",
+  "script-load-wrapper",
+  "primitive-timers",
+  "event-loop-virtualization",
+  "access-lists",
+  "bravojs-init",
+  "bravojs/bravo.js",
+  "bravojs-env",
+  "calculate-capabilities",
+  "bootstrap"
+];
+exports.browser = [
+  "script-load-wrapper",
+  "primitive-timers",
+  "event-loop-virtualization",
+  "access-lists",
+  "bravojs-init",
+  "bravojs/bravo.js",
+  "bravojs-env",
+  "calculate-capabilities",
+  "bootstrap"
+];
+
+exports.native = [
+  "sa-ww-simulation",
+  "script-load-wrapper",
+  "event-loop-virtualization",
+  "access-lists",
+  "bravojs-init",
+  "bravojs/bravo.js",
+  "bravojs-env",
+  "calculate-capabilities",
+  "bootstrap"
+];
