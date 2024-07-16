e2591d8a2351a5ff481eb3529e4509942db4b79f
Jason Erb <jason.erb@sparist.com>
2024-07-16 17:43:57 -0400

    dcp-worker-config.js: improve comment describing form of payment address

--- etc/dcp-worker-config.js
+++ etc/dcp-worker-config.js
@@ -27,7 +27,7 @@
    */
   worker: {
     /* The DCP Bank account where earned funds are deposited by default, of the
-     * form: '0x718cABAabA0d3E85292FD8bCFb78B9f0368d612c' (case is
+     * form: '0x718cABAabA0d3E85292FD8bCFb78B9f0368d612c' (case following 0x is
      * insignificant)
      */
     paymentAddress: undefined,
