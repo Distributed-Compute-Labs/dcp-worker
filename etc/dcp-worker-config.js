9ed119a8501f717fae7187638030db6de67b97ec
Jason Erb <jason.erb@sparist.com>
2024-07-16 17:31:10 -0400

    dcp-worker-config.js: improve comment wording

--- etc/dcp-worker-config.js
+++ etc/dcp-worker-config.js
@@ -3,7 +3,7 @@
  *              Default configuration for the standalone DCP Worker package.
  *              Copy this file before modifying, so that changes are preserved
  *              during the upgrade cycle. If this file is in the "/opt/dcp/.dcp"
- *              directory, copy into the same directory as "dcp-config.js".
+ *              directory, copy as "dcp-config.js" into the same directory.
  *              Otherwise, suggested locations include:
  *              - /etc/dcp/dcp-worker/dcp-config.js, or
  *              - ~/.dcp/dcp-worker/dcp-config.js.
